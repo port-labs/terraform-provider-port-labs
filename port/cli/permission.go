923d55a315b9cf6b85156265787b83230c317390
Amit Shani <amit.shani1995@gmail.com>
2022-08-16 18:17:00 +0300

    fix tests

--- port/cli/permission.go
+++ port/cli/permission.go
@@ -2,12 +2,12 @@ package cli
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 )
 
 func (c *PortClient) CreatePermissions(ctx context.Context, clientID string, scopes ...string) error {
 	url := "v0.1/apps/{app_id}/permissions"
-	responseBody := make(map[string]interface{})
 	resp, err := c.Client.R().
 		SetContext(ctx).
 		SetHeader("Accept", "application/json").
@@ -16,12 +16,16 @@ func (c *PortClient) CreatePermissions(ctx context.Context, clientID string, sco
 		SetBody(map[string]interface{}{
 			"permissions": scopes,
 		}).
-		SetResult(responseBody).
 		Post(url)
 	if err != nil {
 		return err
 	}
-	if !responseBody["ok"].(bool) {
+	b := make(map[string]interface{})
+	err = json.Unmarshal(resp.Body(), &b)
+	if err != nil {
+		return err
+	}
+	if !b["ok"].(bool) {
 		return fmt.Errorf("failed to create permissions: %s", resp.Body())
 	}
 	return nil

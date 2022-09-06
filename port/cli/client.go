0fa91b157437e32ddf41b03f16f444c3961c2707
Amit Benano <Amit.benano777@gmail.com>
2022-09-06 09:53:35 +0300

    change auth

--- port/cli/client.go
+++ port/cli/client.go
@@ -44,10 +44,12 @@ func New(baseURL string, opts ...Option) (*PortClient, error) {
 func (c *PortClient) Authenticate(ctx context.Context, clientID, clientSecret string) (string, error) {
 	url := "v1/auth/access_token"
 	resp, err := c.Client.R().
+		SetBody(map[string]interface{}{
+			"clientId": clientID,
+			"clientSecret": clientSecret,
+		}).
 		SetContext(ctx).
-		SetQueryParam("client_id", clientID).
-		SetQueryParam("client_secret", clientSecret).
-		Get(url)
+		Post(url)
 	if err != nil {
 		return "", err
 	}

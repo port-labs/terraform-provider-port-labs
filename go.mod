34520d1a25634b23d1d6243b70f78dd34932a73b
dvirsegev <dvirsegev@gmail.com>
2023-07-05 10:04:12 +0300

    Create file per type in blueprint

--- go.mod
+++ go.mod
@@ -75,7 +75,6 @@ require (
 	golang.org/x/sys v0.9.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20230525234025-438c736192d0 // indirect
 	google.golang.org/grpc v1.56.1 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
---
+++ port/blueprint/string.go
@@ -0,0 +1,140 @@
+package blueprint
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/port-labs/terraform-provider-port-labs/internal/cli"
+	"github.com/port-labs/terraform-provider-port-labs/internal/flex"
+	"github.com/port-labs/terraform-provider-port-labs/internal/utils"
+)
+
+func addStringPropertiesToState(ctx context.Context, v *cli.BlueprintProperty) *StringPropModel {
+	stringProp := &StringPropModel{
+		MinLength: flex.GoInt64ToFramework(v.MinLength),
+		MaxLength: flex.GoInt64ToFramework(v.MaxLength),
+		Format:    flex.GoStringToFramework(v.Format),
+		Spec:      flex.GoStringToFramework(v.Spec),
+		Pattern:   flex.GoStringToFramework(v.Pattern),
+	}
+
+	if v.Enum != nil {
+		attrs := make([]attr.Value, 0, len(v.Enum))
+		for _, value := range v.Enum {
+			attrs = append(attrs, basetypes.NewStringValue(value.(string)))
+		}
+
+		stringProp.Enum, _ = types.ListValue(types.StringType, attrs)
+	} else {
+		stringProp.Enum = types.ListNull(types.StringType)
+	}
+
+	if v.EnumColors != nil {
+		stringProp.EnumColors, _ = types.MapValueFrom(ctx, types.StringType, v.EnumColors)
+	} else {
+		stringProp.EnumColors = types.MapNull(types.StringType)
+	}
+
+	if v.SpecAuthentication != nil {
+		stringProp.SpecAuthentication = &SpecAuthenticationModel{
+			AuthorizationUrl: types.StringValue(v.SpecAuthentication.AuthorizationUrl),
+			TokenUrl:         types.StringValue(v.SpecAuthentication.TokenUrl),
+			ClientId:         types.StringValue(v.SpecAuthentication.ClientId),
+		}
+	}
+
+	return stringProp
+}
+
+func stringPropResourceToBody(ctx context.Context, state *BlueprintModel, props map[string]cli.BlueprintProperty, required *[]string) error {
+	for propIdentifier, prop := range state.Properties.StringProp {
+		property := cli.BlueprintProperty{
+			Type: "string",
+		}
+
+		if !prop.Title.IsNull() {
+			title := prop.Title.ValueString()
+			property.Title = &title
+		}
+
+		if !prop.Default.IsNull() {
+			property.Default = prop.Default.ValueString()
+		}
+
+		if !prop.Format.IsNull() {
+			format := prop.Format.ValueString()
+			property.Format = &format
+		}
+
+		if !prop.Icon.IsNull() {
+			icon := prop.Icon.ValueString()
+			property.Icon = &icon
+		}
+
+		if !prop.MinLength.IsNull() {
+			minLength := int(prop.MinLength.ValueInt64())
+			property.MinLength = &minLength
+		}
+
+		if !prop.MaxLength.IsNull() {
+			maxLength := int(prop.MaxLength.ValueInt64())
+			property.MaxLength = &maxLength
+		}
+
+		if !prop.Spec.IsNull() {
+			spec := prop.Spec.ValueString()
+			property.Spec = &spec
+		}
+
+		if prop.SpecAuthentication != nil {
+			specAuth := &cli.SpecAuthentication{
+				AuthorizationUrl: prop.SpecAuthentication.AuthorizationUrl.ValueString(),
+				TokenUrl:         prop.SpecAuthentication.TokenUrl.ValueString(),
+				ClientId:         prop.SpecAuthentication.ClientId.ValueString(),
+			}
+			property.SpecAuthentication = specAuth
+		}
+
+		if !prop.Pattern.IsNull() {
+			pattern := prop.Pattern.ValueString()
+			property.Pattern = &pattern
+		}
+
+		if !prop.Description.IsNull() {
+			description := prop.Description.ValueString()
+			property.Description = &description
+		}
+
+		if !prop.Enum.IsNull() {
+			enumList, err := utils.TerraformListToGoArray(ctx, prop.Enum, "string")
+			if err != nil {
+				return err
+			}
+			property.Enum = enumList
+		}
+
+		if !prop.EnumColors.IsNull() {
+			enumColor := map[string]string{}
+			for k, v := range prop.EnumColors.Elements() {
+				value, _ := v.ToTerraformValue(ctx)
+				var keyValue string
+				err := value.As(&keyValue)
+				if err != nil {
+					return err
+				}
+				enumColor[k] = keyValue
+			}
+
+			property.EnumColors = enumColor
+		}
+
+		props[propIdentifier] = property
+
+		if prop.Required.ValueBool() {
+			*required = append(*required, propIdentifier)
+		}
+	}
+	return nil
+}

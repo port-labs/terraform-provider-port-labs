3f23fcaae76f9955b991e763c5543214ed56b966
Dvir Segev <37977923+dvirsegev@users.noreply.github.com>
2023-09-03 13:23:46 +0300

    PORT-4628-terraform-add-examples-of-webhook-and-scorecard-resources (#72)

---
+++ examples/resources/port_scorecard/main.tf
@@ -0,0 +1,18 @@
+resource "port_scorecard" "production_readiness" {
+  identifier = "production-readiness"
+  title      = "Production Readiness"
+  blueprint  = "microservice"
+  rules = [{
+    identifier = "high-avalability"
+    title      = "High Availability"
+    level      = "Gold"
+    query = {
+      combinator = "and"
+      conditions = [{
+        property = "replicaCount"
+        operator = ">="
+        value    = "4"
+      }]
+    }
+  }]
+}
---
+++ examples/resources/port_webhook/main.tf
@@ -0,0 +1,20 @@
+resource "port_webhook" "github" {
+  identifier = "github"
+  title      = "Github"
+  icon       = "Github"
+  enabled    = true
+  mappings = [
+    {
+      "blueprint" : "pullRequest",
+      "filter" : ".headers.\"X-GitHub-Event\" == \"pull_request\"",
+      "entity" : {
+        "identifier" : ".body.pull_request.id | tostring",
+        "title" : ".body.pull_request.title",
+        "properties" : {
+          "author" : ".body.pull_request.user.login",
+          "url" : ".body.pull_request.html_url"
+        }
+      }
+    }
+  ]
+}

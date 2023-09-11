f4d971f48d1c507846cbc959b916c590add9c2dd
dvirsegev <dvirsegev@gmail.com>
2023-09-11 10:48:22 +0300

    Add an example of team resource

--- examples/resources/port_team/main.tf
+++ examples/resources/port_team/main.tf
@@ -1,1 +1,9 @@
-    
+resource "port_team" "example" {
+  name        = "example"
+  description = "example"
+  users = [
+    "user1@test.com",
+    "user2@test.com",
+    "user3@test.com",
+  ]
+}

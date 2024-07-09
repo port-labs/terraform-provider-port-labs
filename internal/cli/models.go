6fe32265a574deb2eccf9e534ff6a41c3ae61357
MatanGevaPort <matang@getport.io>
2024-07-09 11:12:39 +0300

    Added cli models and changed from tfsdk to json in cli

--- internal/cli/models.go
+++ internal/cli/models.go
@@ -21,14 +21,20 @@ type (
 	}
 
 	ScorecardRulesModel struct {
-		Identifier string `tfsdk:"identifier"`
-		Status     string `tfsdk:"status"`
-		Level      string `tfsdk:"level"`
+		Identifier string `json:"identifier"`
+		Status     string `json:"status"`
+		Level      string `json:"level"`
+	}
+
+	ScorecardLevelModel struct {
+		Title string `json:"title"`
+		Color string `json:"color"`
 	}
 
 	ScorecardModel struct {
-		Rules []ScorecardRulesModel `tfsdk:"rules"`
-		Level string                `tfsdk:"level"`
+		Rules  []ScorecardRulesModel `json:"rules"`
+		Levels []ScorecardLevelModel `json:"levels"`
+		Level  string                `json:"level"`
 	}
 
 	Entity struct {
@@ -326,10 +332,11 @@ type (
 
 	Scorecard struct {
 		Meta
-		Identifier string `json:"identifier,omitempty"`
-		Title      string `json:"title,omitempty"`
-		Blueprint  string `json:"blueprint,omitempty"`
-		Rules      []Rule `json:"rules,omitempty"`
+		Identifier string  `json:"identifier,omitempty"`
+		Title      string  `json:"title,omitempty"`
+		Blueprint  string  `json:"blueprint,omitempty"`
+		Levels     []Level `json:"levels,omitempty"`
+		Rules      []Rule  `json:"rules,omitempty"`
 	}
 
 	Rule struct {
@@ -339,6 +346,10 @@ type (
 		Query      Query  `json:"query,omitempty"`
 	}
 
+	Level struct {
+		Title string `json:"title,omitempty"`
+		Color string `json:"color,omitempty"`
+	}
 	Query struct {
 		Combinator string `json:"combinator,omitempty"`
 		Conditions []any  `json:"conditions,omitempty"`

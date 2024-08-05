ede4dec91128e41ecd828ef0c864cc86726b9ace
tankilevitch <tomtankilevitch@gmail.com>
2024-08-05 11:14:29 +0300

    bump version to v21

--- port/action/schema.go
+++ port/action/schema.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 
 	"regexp"
@@ -111,7 +110,6 @@ func ActionSchema() map[string]schema.Attribute {
 					MarkdownDescription: "User properties",
 					Optional:            true,
 					Computed:            true,
-					Default:             objectdefault.StaticValue(types.ObjectValueMust(map[string]attr.Type{}, map[string]attr.Value{})),
 					Attributes: map[string]schema.Attribute{
 						"string_props":  StringPropertySchema(),
 						"number_props":  NumberPropertySchema(),

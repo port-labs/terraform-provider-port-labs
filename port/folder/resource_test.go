fa1da51fb251ab5bfecb7e6a3efc2fda0c33be1b
Matan Oved <matano@getport.io>
2025-02-03 18:09:50 +0200

    PORT-12830-fix tests

--- port/folder/resource_test.go
+++ port/folder/resource_test.go
@@ -12,6 +12,7 @@ import (
 )
 
 func TestAccPortFolderResourceBasicBetaEnabled(t *testing.T) {
+	sidebarIdentifier := utils.GenID()
 	folderIdentifier := utils.GenID()
 	err := os.Setenv("PORT_BETA_FEATURES_ENABLED", "true")
 	if err != nil {
@@ -20,11 +21,12 @@ func TestAccPortFolderResourceBasicBetaEnabled(t *testing.T) {
 	var testAccPortFolderResourceBasic = fmt.Sprintf(`
 
 resource "port_folder" "example_folder" {
-  identifier            = "%s"
-  name                  = "Example Folder"
+  sidebar_identifier    = "%s"
+  folder_identifier     = "%s"
+  title                 = "Example Folder"
   description           = "This is an example folder"
 }
-`, folderIdentifier)
+`, sidebarIdentifier, folderIdentifier)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
@@ -33,8 +35,9 @@ resource "port_folder" "example_folder" {
 			{
 				Config: acctest.ProviderConfig + testAccPortFolderResourceBasic,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("port_folder.example_folder", "identifier", folderIdentifier),
-					resource.TestCheckResourceAttr("port_folder.example_folder", "name", "Example Folder"),
+					resource.TestCheckResourceAttr("port_folder.example_folder", "sidebar_identifier", sidebarIdentifier),
+					resource.TestCheckResourceAttr("port_folder.example_folder", "folder_identifier", folderIdentifier),
+					resource.TestCheckResourceAttr("port_folder.example_folder", "title", "Example Folder"),
 					resource.TestCheckResourceAttr("port_folder.example_folder", "description", "This is an example folder"),
 				),
 			},
@@ -43,6 +46,7 @@ resource "port_folder" "example_folder" {
 }
 
 func TestAccPortFolderResourceBasicBetaDisabled(t *testing.T) {
+	sidebarIdentifier := utils.GenID()
 	folderIdentifier := utils.GenID()
 	err := os.Setenv("PORT_BETA_FEATURES_ENABLED", "false")
 	if err != nil {
@@ -51,11 +55,12 @@ func TestAccPortFolderResourceBasicBetaDisabled(t *testing.T) {
 	var testAccPortFolderResourceBasic = fmt.Sprintf(`
 
 resource "port_folder" "example_folder" {
-  identifier            = "%s"
-  name                  = "Example Folder"
+  sidebar_identifier    = "%s"
+  folder_identifier     = "%s"
+  title                 = "Example Folder"
   description           = "This is an example folder"
 }
-`, folderIdentifier)
+`, sidebarIdentifier, folderIdentifier)
 
 	// expect to fail on beta feature not enabled
 	resource.Test(t, resource.TestCase{
@@ -71,6 +76,7 @@ resource "port_folder" "example_folder" {
 }
 
 func TestAccPortFolderResourceCreateFolderWithParent(t *testing.T) {
+	sidebarIdentifier := utils.GenID()
 	parentFolderIdentifier := utils.GenID()
 	childFolderIdentifier := utils.GenID()
 	err := os.Setenv("PORT_BETA_FEATURES_ENABLED", "true")
@@ -80,21 +86,23 @@ func TestAccPortFolderResourceCreateFolderWithParent(t *testing.T) {
 	var testAccPortFolderResourceParent = fmt.Sprintf(`
 
 resource "port_folder" "parent_folder" {
-  identifier            = "%s"
-  name                  = "Parent Folder"
+  sidebar_identifier    = "%s"
+  folder_identifier     = "%s"
+  title                 = "Parent Folder"
   description           = "This is a parent folder"
 }
-`, parentFolderIdentifier)
+`, sidebarIdentifier, parentFolderIdentifier)
 
 	var testAccPortFolderResourceChild = fmt.Sprintf(`
 
 resource "port_folder" "child_folder" {
-  identifier            = "%s"
-  name                  = "Child Folder"
-  parent                = port_folder.parent_folder.identifier
+  sidebar_identifier    = "%s"
+  folder_identifier     = "%s"
+  parent                = port_folder.parent_folder.folder_identifier
+  title                 = "Child Folder"
   description           = "This is a child folder"
 }
-`, childFolderIdentifier)
+`, sidebarIdentifier, childFolderIdentifier)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
@@ -103,12 +111,14 @@ resource "port_folder" "child_folder" {
 			{
 				Config: acctest.ProviderConfig + testAccPortFolderResourceParent + testAccPortFolderResourceChild,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("port_folder.parent_folder", "identifier", parentFolderIdentifier),
-					resource.TestCheckResourceAttr("port_folder.parent_folder", "name", "Parent Folder"),
+					resource.TestCheckResourceAttr("port_folder.parent_folder", "sidebar_identifier", sidebarIdentifier),
+					resource.TestCheckResourceAttr("port_folder.parent_folder", "folder_identifier", parentFolderIdentifier),
+					resource.TestCheckResourceAttr("port_folder.parent_folder", "title", "Parent Folder"),
 					resource.TestCheckResourceAttr("port_folder.parent_folder", "description", "This is a parent folder"),
-					resource.TestCheckResourceAttr("port_folder.child_folder", "identifier", childFolderIdentifier),
-					resource.TestCheckResourceAttr("port_folder.child_folder", "name", "Child Folder"),
+					resource.TestCheckResourceAttr("port_folder.child_folder", "sidebar_identifier", sidebarIdentifier),
+					resource.TestCheckResourceAttr("port_folder.child_folder", "folder_identifier", childFolderIdentifier),
 					resource.TestCheckResourceAttr("port_folder.child_folder", "parent", parentFolderIdentifier),
+					resource.TestCheckResourceAttr("port_folder.child_folder", "title", "Child Folder"),
 					resource.TestCheckResourceAttr("port_folder.child_folder", "description", "This is a child folder"),
 				),
 			},

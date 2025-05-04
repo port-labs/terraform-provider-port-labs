095e10723b2bb659878f241b092e1e6df75e99a5
Eyal Halpern Shalev <eyalh@getport.io>
2025-05-04 22:53:00 +0300

    When refreshing permission lists, use the ordering of values from the previous state for sorting values from the server

--- port/blueprint-permissions/refreshBluePrintPermissionsToState.go
+++ port/blueprint-permissions/refreshBluePrintPermissionsToState.go
@@ -1,37 +1,74 @@
 package blueprint_permissions
 
 import (
+	"github.com/port-labs/terraform-provider-port-labs/v2/internal/utils"
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/port-labs/terraform-provider-port-labs/v2/internal/cli"
 )
 
-func goStringListToTFList(list []string) []types.String {
-	var result = make([]types.String, len(list))
-	for i, u := range list {
-		result[i] = types.StringValue(u)
-	}
-
-	return result
-}
-
-func blueprintPermissionsBlockToBlueprintPermissionsTFBlock(block cli.BlueprintPermissionsBlock) *BlueprintPermissionsTFBlock {
-	return &BlueprintPermissionsTFBlock{
-		Users:       goStringListToTFList(block.Users),
-		Roles:       goStringListToTFList(block.Roles),
-		Teams:       goStringListToTFList(block.Teams),
-		OwnedByTeam: types.BoolValue(*block.OwnedByTeam),
-	}
-}
-
 func refreshBlueprintPermissionsState(state *BlueprintPermissionsModel, a *cli.BlueprintPermissions, blueprintId string) error {
+	oldPermissions := state.Entities
+	if oldPermissions == nil {
+		oldPermissions = &EntitiesBlueprintPermissionsModel{}
+	}
+
 	state.ID = types.StringValue(blueprintId)
 	state.BlueprintIdentifier = types.StringValue(blueprintId)
 	state.Entities = &EntitiesBlueprintPermissionsModel{}
-	state.Entities.Update = blueprintPermissionsBlockToBlueprintPermissionsTFBlock(a.Entities.Update)
-	state.Entities.Unregister = blueprintPermissionsBlockToBlueprintPermissionsTFBlock(a.Entities.Unregister)
-	state.Entities.Register = blueprintPermissionsBlockToBlueprintPermissionsTFBlock(a.Entities.Register)
+
+	if oldPermissions.Update == nil {
+		state.Entities.Update = &BlueprintPermissionsTFBlock{
+			Users:       utils.Map(a.Entities.Update.Users, types.StringValue),
+			Roles:       utils.Map(a.Entities.Update.Roles, types.StringValue),
+			Teams:       utils.Map(a.Entities.Update.Teams, types.StringValue),
+			OwnedByTeam: types.BoolValue(*a.Entities.Update.OwnedByTeam),
+		}
+	} else {
+		state.Entities.Update = &BlueprintPermissionsTFBlock{
+			Users:       utils.Map(utils.SortStringSliceByOther(a.Entities.Update.Users, utils.TFStringListToStringArray(oldPermissions.Update.Users)), types.StringValue),
+			Roles:       utils.Map(utils.SortStringSliceByOther(a.Entities.Update.Roles, utils.TFStringListToStringArray(oldPermissions.Update.Roles)), types.StringValue),
+			Teams:       utils.Map(utils.SortStringSliceByOther(a.Entities.Update.Teams, utils.TFStringListToStringArray(oldPermissions.Update.Teams)), types.StringValue),
+			OwnedByTeam: types.BoolValue(*a.Entities.Update.OwnedByTeam),
+		}
+	}
+
+	if oldPermissions.Unregister == nil {
+		state.Entities.Unregister = &BlueprintPermissionsTFBlock{
+			Users:       utils.Map(a.Entities.Unregister.Users, types.StringValue),
+			Roles:       utils.Map(a.Entities.Unregister.Roles, types.StringValue),
+			Teams:       utils.Map(a.Entities.Unregister.Teams, types.StringValue),
+			OwnedByTeam: types.BoolValue(*a.Entities.Unregister.OwnedByTeam),
+		}
+	} else {
+		state.Entities.Unregister = &BlueprintPermissionsTFBlock{
+			Users:       utils.Map(utils.SortStringSliceByOther(a.Entities.Update.Users, utils.TFStringListToStringArray(oldPermissions.Unregister.Users)), types.StringValue),
+			Roles:       utils.Map(utils.SortStringSliceByOther(a.Entities.Update.Roles, utils.TFStringListToStringArray(oldPermissions.Unregister.Roles)), types.StringValue),
+			Teams:       utils.Map(utils.SortStringSliceByOther(a.Entities.Update.Teams, utils.TFStringListToStringArray(oldPermissions.Unregister.Teams)), types.StringValue),
+			OwnedByTeam: types.BoolValue(*a.Entities.Unregister.OwnedByTeam),
+		}
+	}
+
+	if oldPermissions.Register == nil {
+		state.Entities.Register = &BlueprintPermissionsTFBlock{
+			Users:       utils.Map(a.Entities.Register.Users, types.StringValue),
+			Roles:       utils.Map(a.Entities.Register.Roles, types.StringValue),
+			Teams:       utils.Map(a.Entities.Register.Teams, types.StringValue),
+			OwnedByTeam: types.BoolValue(*a.Entities.Register.OwnedByTeam),
+		}
+	} else {
+		state.Entities.Register = &BlueprintPermissionsTFBlock{
+			Users:       utils.Map(utils.SortStringSliceByOther(a.Entities.Update.Users, utils.TFStringListToStringArray(oldPermissions.Register.Users)), types.StringValue),
+			Roles:       utils.Map(utils.SortStringSliceByOther(a.Entities.Update.Roles, utils.TFStringListToStringArray(oldPermissions.Register.Roles)), types.StringValue),
+			Teams:       utils.Map(utils.SortStringSliceByOther(a.Entities.Update.Teams, utils.TFStringListToStringArray(oldPermissions.Register.Teams)), types.StringValue),
+			OwnedByTeam: types.BoolValue(*a.Entities.Register.OwnedByTeam),
+		}
+	}
+
+	if oldPermissions.UpdateProperties == nil {
+		oldPermissions.UpdateProperties = &BlueprintRelationsPermissionsTFBlock{}
+	}
 
 	state.Entities.UpdateProperties = nil
 	var mappedUpdateProperties BlueprintRelationsPermissionsTFBlock = nil
@@ -39,7 +76,37 @@ func refreshBlueprintPermissionsState(state *BlueprintPermissionsModel, a *cli.B
 		state.Entities.UpdateMetadataProperties = &BlueprintMetadataPermissionsTFBlock{}
 		mappedUpdateProperties = make(BlueprintRelationsPermissionsTFBlock)
 		for updatePropertyKey, updatePropertyValue := range a.Entities.UpdateProperties {
-			var current = blueprintPermissionsBlockToBlueprintPermissionsTFBlock(updatePropertyValue)
+			var oldPropValue *BlueprintPermissionsTFBlock
+			if strings.HasPrefix(updatePropertyKey, "$") {
+				switch updatePropertyKey {
+				case "$title":
+					oldPropValue = oldPermissions.UpdateMetadataProperties.Title
+				case "$identifier":
+					oldPropValue = oldPermissions.UpdateMetadataProperties.Identifier
+				case "$icon":
+					oldPropValue = oldPermissions.UpdateMetadataProperties.Icon
+				case "$team":
+					oldPropValue = oldPermissions.UpdateMetadataProperties.Team
+				}
+			} else if val, ok := (*oldPermissions.UpdateProperties)[updatePropertyKey]; ok {
+				oldPropValue = &val
+			}
+			var current *BlueprintPermissionsTFBlock
+			if oldPropValue == nil {
+				current = &BlueprintPermissionsTFBlock{
+					Users:       utils.Map(updatePropertyValue.Users, types.StringValue),
+					Roles:       utils.Map(updatePropertyValue.Roles, types.StringValue),
+					Teams:       utils.Map(updatePropertyValue.Teams, types.StringValue),
+					OwnedByTeam: types.BoolValue(*updatePropertyValue.OwnedByTeam),
+				}
+			} else {
+				current = &BlueprintPermissionsTFBlock{
+					Users:       utils.Map(utils.SortStringSliceByOther(updatePropertyValue.Users, utils.TFStringListToStringArray(oldPropValue.Users)), types.StringValue),
+					Roles:       utils.Map(utils.SortStringSliceByOther(updatePropertyValue.Roles, utils.TFStringListToStringArray(oldPropValue.Roles)), types.StringValue),
+					Teams:       utils.Map(utils.SortStringSliceByOther(updatePropertyValue.Teams, utils.TFStringListToStringArray(oldPropValue.Teams)), types.StringValue),
+					OwnedByTeam: types.BoolValue(*updatePropertyValue.OwnedByTeam),
+				}
+			}
 
 			if strings.HasPrefix(updatePropertyKey, "$") {
 				switch updatePropertyKey {
@@ -60,10 +127,26 @@ func refreshBlueprintPermissionsState(state *BlueprintPermissionsModel, a *cli.B
 			state.Entities.UpdateProperties = &mappedUpdateProperties
 		}
 	}
+
 	if len(a.Entities.UpdateRelations) > 0 {
 		var mappedUpdateRelations = make(BlueprintRelationsPermissionsTFBlock, len(a.Entities.UpdateRelations))
 		for updateRelationKey, updateRelationValue := range a.Entities.UpdateRelations {
-			mappedUpdateRelations[updateRelationKey] = *blueprintPermissionsBlockToBlueprintPermissionsTFBlock(updateRelationValue)
+			oldRelValue, hasOldRelValue := (*oldPermissions.UpdateRelations)[updateRelationKey]
+			if !hasOldRelValue {
+				mappedUpdateRelations[updateRelationKey] = BlueprintPermissionsTFBlock{
+					Users:       utils.Map(updateRelationValue.Users, types.StringValue),
+					Roles:       utils.Map(updateRelationValue.Roles, types.StringValue),
+					Teams:       utils.Map(updateRelationValue.Teams, types.StringValue),
+					OwnedByTeam: types.BoolValue(*updateRelationValue.OwnedByTeam),
+				}
+			} else {
+				mappedUpdateRelations[updateRelationKey] = BlueprintPermissionsTFBlock{
+					Users:       utils.Map(utils.SortStringSliceByOther(updateRelationValue.Users, utils.TFStringListToStringArray(oldRelValue.Users)), types.StringValue),
+					Roles:       utils.Map(utils.SortStringSliceByOther(updateRelationValue.Roles, utils.TFStringListToStringArray(oldRelValue.Roles)), types.StringValue),
+					Teams:       utils.Map(utils.SortStringSliceByOther(updateRelationValue.Teams, utils.TFStringListToStringArray(oldRelValue.Teams)), types.StringValue),
+					OwnedByTeam: types.BoolValue(*updateRelationValue.OwnedByTeam),
+				}
+			}
 		}
 		state.Entities.UpdateRelations = &mappedUpdateRelations
 	} else {
--- port/page-permissions/refreshPagePermissionsToState.go
+++ port/page-permissions/refreshPagePermissionsToState.go
@@ -3,27 +3,18 @@ package page_permissions
 import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/port-labs/terraform-provider-port-labs/v2/internal/cli"
+	"github.com/port-labs/terraform-provider-port-labs/v2/internal/utils"
 )
 
 func refreshPagePermissionsState(state *PagePermissionsModel, a *cli.PagePermissions, pageId string) error {
+	oldPermissions := state.Read
 	state.ID = types.StringValue(pageId)
 	state.PageIdentifier = types.StringValue(pageId)
 	state.Read = &ReadPagePermissionsModel{}
 
-	state.Read.Users = make([]types.String, len(a.Read.Users))
-	for i, u := range a.Read.Users {
-		state.Read.Users[i] = types.StringValue(u)
-	}
-
-	state.Read.Roles = make([]types.String, len(a.Read.Roles))
-	for i, u := range a.Read.Roles {
-		state.Read.Roles[i] = types.StringValue(u)
-	}
-
-	state.Read.Teams = make([]types.String, len(a.Read.Teams))
-	for i, u := range a.Read.Teams {
-		state.Read.Teams[i] = types.StringValue(u)
-	}
+	state.Read.Users = utils.Map(utils.SortStringSliceByOther(a.Read.Users, utils.TFStringListToStringArray(oldPermissions.Users)), types.StringValue)
+	state.Read.Roles = utils.Map(utils.SortStringSliceByOther(a.Read.Roles, utils.TFStringListToStringArray(oldPermissions.Roles)), types.StringValue)
+	state.Read.Teams = utils.Map(utils.SortStringSliceByOther(a.Read.Teams, utils.TFStringListToStringArray(oldPermissions.Teams)), types.StringValue)
 
 	return nil
 }

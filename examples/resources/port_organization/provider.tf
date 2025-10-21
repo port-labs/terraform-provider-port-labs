83c3fb8d598109ebcac1c1daedbb74a26f50c1b9
azeezport <azeez.akinkunmi@port.io>
2025-10-21 22:42:37 +0100

    replace provider.tf with symbolic link to shared configuration

--- examples/resources/port_organization/provider.tf
+++ examples/resources/port_organization/provider.tf
@@ -1,14 +1,1 @@
-terraform {
-  required_providers {
-    port = {
-      source  = "port-labs/port-labs"
-      version = "~> 2"
-    }
-  }
-}
-
-provider "port" {
-  client_id = "" # or set the environment variable PORT_CLIENT_ID
-  secret    = "" # or set the environment variable PORT_CLIENT_SECRET
-  base_url  = "https://api.getport.io"
-}
+../../provider.tf

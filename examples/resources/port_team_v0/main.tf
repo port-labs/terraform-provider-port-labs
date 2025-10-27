719ac2ff4c5c8b924d8bc88967278671826594c7
Itamar Smirra <itamars@getport.io>
2025-10-27 10:25:50 +0200

    fix team updates by using the old team name in the url

--- examples/resources/port_team_v0/main.tf
+++ examples/resources/port_team_v0/main.tf
@@ -1,20 +1,23 @@
 resource "port_team" "Example-Team" {
   name        = "Example Team"
-  description = "An example teammmmmmm"
+  description = "An example team"
   # Note, this will need real users to work!
   users = [
+    "user1@test.com",
+    "user2@test.com",
+    "user3@test.com",
   ]
 }
 
-# resource "port_blueprint" "some-blueprint" {
-#   identifier = "some-blueprint"
-#   title      = "Some Blueprint"
-# }
+resource "port_blueprint" "some-blueprint" {
+  identifier = "some-blueprint"
+  title      = "Some Blueprint"
+}
 
-# resource "port_entity" "some-entity" {
-#   blueprint = port_blueprint.some-blueprint.identifier
-#   title = "Some Entity"
-#   teams = [
-#     port_team.Example-Team.name,
-#   ]
-# }
+resource "port_entity" "some-entity" {
+  blueprint = port_blueprint.some-blueprint.identifier
+  title = "Some Entity"
+  teams = [
+    port_team.Example-Team.name,
+  ]
+}

77d1fb1e5ec97820183d793fb8029770d312f673
matanl <matanlevi@getport.io>
2026-01-21 13:01:55 +0200

    Make the widget in page to be list instead of string array in order to support unknown type on plan stage

--- port/page/model.go
+++ port/page/model.go
@@ -3,20 +3,20 @@ package page
 import "github.com/hashicorp/terraform-plugin-framework/types"
 
 type PageModel struct {
-	ID          types.String   `tfsdk:"id"`
-	Identifier  types.String   `tfsdk:"identifier"`
-	Title       types.String   `tfsdk:"title"`
-	Type        types.String   `tfsdk:"type"`
-	Parent      types.String   `tfsdk:"parent"`
-	After       types.String   `tfsdk:"after"`
-	Icon        types.String   `tfsdk:"icon"`
-	Locked      types.Bool     `tfsdk:"locked"`
-	Blueprint   types.String   `tfsdk:"blueprint"`
-	Widgets     []types.String `tfsdk:"widgets"`
-	PageFilters []types.String `tfsdk:"page_filters"`
-	CreatedAt   types.String   `tfsdk:"created_at"`
-	CreatedBy   types.String   `tfsdk:"created_by"`
-	UpdatedAt   types.String   `tfsdk:"updated_at"`
-	UpdatedBy   types.String   `tfsdk:"updated_by"`
-	Description types.String   `tfsdk:"description"`
+	ID          types.String `tfsdk:"id"`
+	Identifier  types.String `tfsdk:"identifier"`
+	Title       types.String `tfsdk:"title"`
+	Type        types.String `tfsdk:"type"`
+	Parent      types.String `tfsdk:"parent"`
+	After       types.String `tfsdk:"after"`
+	Icon        types.String `tfsdk:"icon"`
+	Locked      types.Bool   `tfsdk:"locked"`
+	Blueprint   types.String `tfsdk:"blueprint"`
+	Widgets     types.List   `tfsdk:"widgets"`
+	PageFilters types.List   `tfsdk:"page_filters"`
+	CreatedAt   types.String `tfsdk:"created_at"`
+	CreatedBy   types.String `tfsdk:"created_by"`
+	UpdatedAt   types.String `tfsdk:"updated_at"`
+	UpdatedBy   types.String `tfsdk:"updated_by"`
+	Description types.String `tfsdk:"description"`
 }

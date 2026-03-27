4fa177a21d0a9b576f1bd58f3f52833afc33599b
azeezport <azeez.akinkunmi@port.io>
2026-03-27 11:28:58 +0100

    feat: expand port_organization resource with full API settings support
    - Add settings fields: hidden_blueprints, federated_logout, portal_icon,
      portal_title, support_user_permission, support_user_ttl,
      support_user_expires_at, port_agent_streamer_name,
      include_blueprints_in_global_search_by_default
    - Add announcement fields: enabled, content, link, color
    - Add top-level fields: is_onboarded, tool_selection_provisioning_status
    - Nest hiddenBlueprints under settings object to match API contract
    - Add acceptance tests for settings and announcement
    - Regenerate docs with all 16 attributes

--- port/organization/organizationSchema.go
+++ port/organization/organizationSchema.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 func OrganizationSchema() map[string]schema.Attribute {
@@ -15,6 +16,81 @@ func OrganizationSchema() map[string]schema.Attribute {
 		},
 		"name": schema.StringAttribute{
 			MarkdownDescription: "The name of the organization",
+			Optional:            true,
+			Computed:            true,
+		},
+		"hidden_blueprints": schema.ListAttribute{
+			MarkdownDescription: "A list of blueprint identifiers to hide from the portal",
+			Optional:            true,
+			ElementType:         types.StringType,
+		},
+		"federated_logout": schema.BoolAttribute{
+			MarkdownDescription: "Enable or disable federated logout",
+			Optional:            true,
+			Computed:            true,
+		},
+		"portal_icon": schema.StringAttribute{
+			MarkdownDescription: "The icon URI for the portal (must be a valid URI)",
+			Optional:            true,
+			Computed:            true,
+		},
+		"portal_title": schema.StringAttribute{
+			MarkdownDescription: "The title for the portal",
+			Optional:            true,
+			Computed:            true,
+		},
+		"support_user_permission": schema.StringAttribute{
+			MarkdownDescription: "The support user permission level (e.g. OPT_OUT)",
+			Optional:            true,
+			Computed:            true,
+		},
+		"support_user_ttl": schema.StringAttribute{
+			MarkdownDescription: "The TTL for support user access (e.g. ONE_DAY)",
+			Optional:            true,
+			Computed:            true,
+		},
+		"support_user_expires_at": schema.StringAttribute{
+			MarkdownDescription: "The expiration date for support user access (ISO 8601 format)",
+			Optional:            true,
+			Computed:            true,
+		},
+		"port_agent_streamer_name": schema.StringAttribute{
+			MarkdownDescription: "The name of the Port agent streamer (e.g. KAFKA)",
+			Optional:            true,
+			Computed:            true,
+		},
+		"include_blueprints_in_global_search_by_default": schema.BoolAttribute{
+			MarkdownDescription: "Whether to include blueprints in global search by default",
+			Optional:            true,
+			Computed:            true,
+		},
+		"is_onboarded": schema.BoolAttribute{
+			MarkdownDescription: "Whether the organization has completed onboarding",
+			Optional:            true,
+			Computed:            true,
+		},
+		"tool_selection_provisioning_status": schema.StringAttribute{
+			MarkdownDescription: "The status of tool selection provisioning (e.g. IN_PROGRESS)",
+			Optional:            true,
+			Computed:            true,
+		},
+		"announcement_enabled": schema.BoolAttribute{
+			MarkdownDescription: "Whether the organization announcement is enabled",
+			Optional:            true,
+			Computed:            true,
+		},
+		"announcement_content": schema.StringAttribute{
+			MarkdownDescription: "The content of the organization announcement",
+			Optional:            true,
+			Computed:            true,
+		},
+		"announcement_link": schema.StringAttribute{
+			MarkdownDescription: "A link for the organization announcement",
+			Optional:            true,
+			Computed:            true,
+		},
+		"announcement_color": schema.StringAttribute{
+			MarkdownDescription: "The color of the organization announcement (e.g. blue)",
 			Optional:            true,
 			Computed:            true,
 		},
--- port/organization/organization_resource_test.go
+++ port/organization/organization_resource_test.go
@@ -63,6 +63,154 @@ func TestAccPortOrganizationUpdate(t *testing.T) {
 	})
 }
 
+func TestAccPortOrganizationWithHiddenBlueprints(t *testing.T) {
+	orgName := utils.GenID()
+	bp1 := utils.GenID()
+	bp2 := utils.GenID()
+	var testAccOrganizationConfigCreate = fmt.Sprintf(`
+	resource "port_blueprint" "bp1" {
+		identifier = "%s"
+		title      = "BP1"
+		icon       = "Microservice"
+	}
+	resource "port_blueprint" "bp2" {
+		identifier = "%s"
+		title      = "BP2"
+		icon       = "Microservice"
+	}
+	resource "port_organization" "test" {
+		name              = "%s"
+		hidden_blueprints = [port_blueprint.bp1.identifier, port_blueprint.bp2.identifier]
+	}`, bp1, bp2, orgName)
+
+	var testAccOrganizationConfigUpdate = fmt.Sprintf(`
+	resource "port_blueprint" "bp1" {
+		identifier = "%s"
+		title      = "BP1"
+		icon       = "Microservice"
+	}
+	resource "port_blueprint" "bp2" {
+		identifier = "%s"
+		title      = "BP2"
+		icon       = "Microservice"
+	}
+	resource "port_organization" "test" {
+		name              = "%s"
+		hidden_blueprints = [port_blueprint.bp1.identifier]
+	}`, bp1, bp2, orgName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: acctest.ProviderConfig + testAccOrganizationConfigCreate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("port_organization.test", "name", orgName),
+					resource.TestCheckResourceAttr("port_organization.test", "hidden_blueprints.#", "2"),
+				),
+			},
+			{
+				Config: acctest.ProviderConfig + testAccOrganizationConfigUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("port_organization.test", "name", orgName),
+					resource.TestCheckResourceAttr("port_organization.test", "hidden_blueprints.#", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccPortOrganizationWithSettings(t *testing.T) {
+	orgName := utils.GenID()
+	var testAccOrganizationConfigCreate = fmt.Sprintf(`
+	resource "port_organization" "test" {
+		name          = "%s"
+		portal_title  = "My Dev Portal"
+		portal_icon   = "https://example.com/icon.png"
+		include_blueprints_in_global_search_by_default = true
+	}`, orgName)
+
+	var testAccOrganizationConfigUpdate = fmt.Sprintf(`
+	resource "port_organization" "test" {
+		name          = "%s"
+		portal_title  = "Updated Portal"
+		portal_icon   = "https://example.com/icon2.png"
+		include_blueprints_in_global_search_by_default = false
+	}`, orgName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: acctest.ProviderConfig + testAccOrganizationConfigCreate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("port_organization.test", "name", orgName),
+					resource.TestCheckResourceAttr("port_organization.test", "portal_title", "My Dev Portal"),
+					resource.TestCheckResourceAttr("port_organization.test", "portal_icon", "https://example.com/icon.png"),
+					resource.TestCheckResourceAttr("port_organization.test", "include_blueprints_in_global_search_by_default", "true"),
+				),
+			},
+			{
+				Config: acctest.ProviderConfig + testAccOrganizationConfigUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("port_organization.test", "name", orgName),
+					resource.TestCheckResourceAttr("port_organization.test", "portal_title", "Updated Portal"),
+					resource.TestCheckResourceAttr("port_organization.test", "portal_icon", "https://example.com/icon2.png"),
+					resource.TestCheckResourceAttr("port_organization.test", "include_blueprints_in_global_search_by_default", "false"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccPortOrganizationWithAnnouncement(t *testing.T) {
+	orgName := utils.GenID()
+	var testAccOrganizationConfigCreate = fmt.Sprintf(`
+	resource "port_organization" "test" {
+		name                  = "%s"
+		announcement_enabled  = true
+		announcement_content  = "Welcome to the portal"
+		announcement_link     = "https://example.com"
+		announcement_color    = "blue"
+	}`, orgName)
+
+	var testAccOrganizationConfigUpdate = fmt.Sprintf(`
+	resource "port_organization" "test" {
+		name                  = "%s"
+		announcement_enabled  = false
+		announcement_content  = "Portal under maintenance"
+		announcement_color    = "red"
+	}`, orgName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: acctest.ProviderConfig + testAccOrganizationConfigCreate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("port_organization.test", "name", orgName),
+					resource.TestCheckResourceAttr("port_organization.test", "announcement_enabled", "true"),
+					resource.TestCheckResourceAttr("port_organization.test", "announcement_content", "Welcome to the portal"),
+					resource.TestCheckResourceAttr("port_organization.test", "announcement_link", "https://example.com"),
+					resource.TestCheckResourceAttr("port_organization.test", "announcement_color", "blue"),
+				),
+			},
+			{
+				Config: acctest.ProviderConfig + testAccOrganizationConfigUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("port_organization.test", "name", orgName),
+					resource.TestCheckResourceAttr("port_organization.test", "announcement_enabled", "false"),
+					resource.TestCheckResourceAttr("port_organization.test", "announcement_content", "Portal under maintenance"),
+					resource.TestCheckResourceAttr("port_organization.test", "announcement_color", "red"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccPortOrganizationImport(t *testing.T) {
 	orgName := utils.GenID()
 	var testAccOrganizationConfigCreate = fmt.Sprintf(`
